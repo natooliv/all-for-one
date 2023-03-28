c58d006f651dfe68ff8284cd917f376e985d3949
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-03-28 16:58:13 -0300

    Requisito 17 ao 19

---
+++ desafio19.sql
@@ -0,0 +1,3 @@
+-- 9 - Mostre a quantidade de pedidos que foram feitos na tabela orders pelo employee_id igual a 5 ou 6, e que foram enviados através do método(coluna) shipper_id igual a 2.
+
+SELECT COUNT(employee_id) FROM orders WHERE employee_id IN (5,6) AND shipper_id = 2;
