b92844ab0f7685922110afb37873e4759ee4e6fa
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-03-28 17:17:13 -0300

    Requisito 20 ao 22

---
+++ desafio20.sql
@@ -0,0 +1,3 @@
+-- Adicione à tabela order_details um registro com order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0
+INSERT INTO order_details(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+  VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129)
---
+++ desafio22.sql
@@ -0,0 +1,2 @@
+-- Req22 - Atualize todos os dados da coluna 'discount', na tabela 'order_details', para 15.
+UPDATE order_details SET discount = 15;
